55f476e3b93d815a31d59d82bef9b5d5da4a31e0
nitronoid <jackdiver@hotmail.co.uk>
2019-05-17 00:27:35 +0100

    Re-read paper and corrected beta-feature issues.

--- src/split/device/color/beta_feature.cu
+++ src/split/device/color/beta_feature.cu
@@ -1,5 +1,6 @@
 #include "split/device/color/beta_feature.cuh"
 #include "split/device/detail/zip_it.cuh"
+#include "split/device/detail/unary_functional.cuh"
 
 SPLIT_DEVICE_NAMESPACE_BEGIN
 
@@ -9,11 +10,15 @@ namespace
 {
 struct lab_to_beta
 {
-  lab_to_beta(real Lmin, real Lmax, real mu)
-    : Lmin(std::move(Lmin)), Lmax(std::move(Lmax)), mu(std::move(mu))
+  lab_to_beta(real lambda, real Lmin, real Lmax, real mu)
+    : lambda(std::move(lambda))
+    , Lmin(std::move(Lmin))
+    , Lmax(std::move(Lmax))
+    , mu(std::move(mu))
   {
   }
 
+  real lambda;
   real Lmin;
   real Lmax;
   real mu;
@@ -24,7 +29,6 @@ struct lab_to_beta
     const real L = lab.get<0>();
     const real a = abs(lab.get<1>());
     const real b = abs(lab.get<2>());
-    const real lambda = 0.2f * max(a, b);
 
     real beta = 0.f;
 
@@ -46,10 +50,19 @@ beta_feature(cusp::array2d<real, cusp::device_memory>::const_view di_lab_points,
              cusp::array1d<real, cusp::device_memory>::view do_beta,
              const real mu)
 {
+  const int npoints = di_lab_points.num_cols;
   // Find the maximum luminance
   const auto luminance = di_lab_points.row(0);
   const auto luminance_max =
     *thrust::max_element(luminance.begin(), luminance.end());
+
+  // Read the absolute chroma values
+  const auto chroma_begin = thrust::make_transform_iterator(
+    di_lab_points.row(1).begin(), detail::unary_abs<real>());
+  const auto chroma_end = chroma_begin + npoints;
+  // Find the lambda value, as the maximum absolute chroma * 0.2
+  const auto lambda = 0.2f * (*thrust::max_element(chroma_begin, chroma_end));
+
   // Compute the upper and lower bounds for the beta feature
   const real lo = luminance_max * 0.20f;
   const real hi = luminance_max * 0.95f;
@@ -58,10 +71,10 @@ beta_feature(cusp::array2d<real, cusp::device_memory>::const_view di_lab_points,
   auto lab_begin = detail::zip_it(di_lab_points.row(0).begin(),
                                   di_lab_points.row(1).begin(),
                                   di_lab_points.row(2).begin());
-  auto lab_end = lab_begin + di_lab_points.num_cols;
+  auto lab_end = lab_begin + npoints;
 
   thrust::transform(
-    lab_begin, lab_end, do_beta.begin(), lab_to_beta(lo, hi, mu));
+    lab_begin, lab_end, do_beta.begin(), lab_to_beta(lambda, lo, hi, mu));
 }
 
 }  // namespace color
