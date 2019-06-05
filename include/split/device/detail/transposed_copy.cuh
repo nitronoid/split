b1a5d067ba75fff8f4ae65423956633a32eeb9a9
Jack Diver <s4902673@bournemouth.ac.uk>
2019-06-05 20:25:23 +0100

    Marked the transposed copy function as inline to prevent violation of the ODR.

--- include/split/device/detail/transposed_copy.cuh
+++ include/split/device/detail/transposed_copy.cuh
@@ -9,10 +9,10 @@ SPLIT_DEVICE_NAMESPACE_BEGIN
 
 namespace detail
 {
-void transposed_copy(const int m,
-                     const int n,
-                     cusp::array1d<int, cusp::device_memory>::view A,
-                     cusp::array1d<int, cusp::device_memory>::view At)
+inline void transposed_copy(const int m,
+                            const int n,
+                            cusp::array1d<int, cusp::device_memory>::view A,
+                            cusp::array1d<int, cusp::device_memory>::view At)
 {
   auto indices = make_transpose_iterator(m, n);
   thrust::scatter(A.begin(), A.end(), indices, At.begin());
